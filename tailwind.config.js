fa0c2f671df554d0f02cb2ead2c2a09a00bc6bea
Marek <ciesla.marek@gmail.com>
2024-01-27 16:17:37 +0100

    added extended color and font for tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'wtf': '#557788'
+      },
+      fontFamily: {
+        'popins': ['Poppins', 'sans-serif' ],
+      }
+    },
   },
   plugins: [],
 }
