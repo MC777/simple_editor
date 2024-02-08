51aaef328f5ad8004e708ae27104384bbcee6987
Marek <ciesla.marek@gmail.com>
2024-02-08 17:07:19 +0100

    added wiggle animation for flopy icon

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,20 @@ module.exports = {
   ],
   theme: {
     extend: {
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+      },
       colors: {
         'wtf': '#557788'
       },
       fontFamily: {
         'popins': ['Poppins', 'sans-serif' ],
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        }
       }
     },
   },
